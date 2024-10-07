06848d49e386b10db35d6b92e702c748cbda7aff
TLowe248 <105463078+TLowe248@users.noreply.github.com>
2024-10-07 14:14:31 -0500

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'taskit';
+  tasks: String[] = [];
+  newTask: String = "";
+  strikes: number[] = [];
+  addTask() {
+	this.tasks.push(this.newTask);
+	this.newTask = "";
+	localStorage.setItem("tasks", JSON.stringify(this.tasks));
+	localStorage.setItem("strikes", JSON.stringify(this.strikes));
+  }
+  deleteTask(position: number) {
+	if (!this.strikes.includes(position)) {
+		this.strikes.push(position);
+	} else {
+		this.tasks.splice(position, 1);
+		this.strikes = this.strikes.filter((value) => value != position);
+	}
+	localStorage.setItem("tasks", JSON.stringify(this.tasks));
+	localStorage.setItem("strikes", JSON.stringify(this.strikes));
+  }
+
+  clearTasks() {
+	this.tasks = [];
+	this.strikes = [];
+	localStorage.setItem("tasks", JSON.stringify(this.tasks));
+	localStorage.setItem("strikes", JSON.stringify(this.strikes));
+  }
+
+  ngOnInit(): void {
+	let task_json = localStorage.getItem("tasks");
+	if (task_json !== null) {
+		this.tasks = JSON.parse(task_json);
+	}
+	let strikes_json = localStorage.getItem("strikes");
+	if (strikes_json !== null) {
+		this.strikes = JSON.parse(strikes_json);
+	}
+  }
 }
